0b85b8e4fc73ce285962c5d568ea73dbe95b7c8a
Lingqizhu <lingqizhu1980@gmail.com>
2022-02-25 10:17:51 +0000

    all works

--- src/login.js
+++ src/login.js
@@ -5,8 +5,6 @@ import "./login.css";
 function Login(props) {
   const [disabled, cDisabled] = useState(false);
   const navigate = useNavigate();
-  /* const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState(""); */
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -25,19 +23,6 @@ function Login(props) {
       })
 
   };
-  /* const submitHandler = (e) => {
-    console.log("submit");
-    e.preventDefault();
-    props.client
-      .login(userName, password)
-      .then((response) => {
-        loggedIn(response.data.token);
-      })
-      .catch((err) => {
-        alert("an error has occurred");
-        console.log(err);
-      });
-  }; */
 
   return (
     <div>
@@ -46,22 +31,10 @@ function Login(props) {
           <h2>Event App</h2>
           <h4>Login</h4>
           <input type="text" name="username" placeholder="username" id="loginput" disabled={disabled} />
-          {/* <input
-            type="text"
-            placeholder="username"
-            id="loginput"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-          /> */}
+
           <br />
           <input type="password" name="password" placeholder="password" id="loginput" disabled={disabled} />
-         {/*  <input
-            type="password"
-            placeholder="password"
-            id="loginput"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          /> */}
+
           <br />
           <br />
           <button type="submit" className="submit" disabled={disabled}>
